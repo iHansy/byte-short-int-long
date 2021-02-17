0012509467cfd224ebc9a44c130fe08943e2d8da
Hans Accola <Hansolo6283@gmail.com>
2021-02-16 22:51:38 -0600

    learned about max/min integer values

--- src/com/hansy/Main.java
+++ src/com/hansy/Main.java
@@ -6,5 +6,14 @@ public class Main {
 
         int myValue = 10000;
 
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println("Busted MAX Value = " + (myMaxIntValue + 1));
+        System.out.println("Busted MIN Value = " + (myMinIntValue - 1));
+
+        int myMaxIntTest = 2147483647;
+        //can also write numbers with _ underscores as a comma
     }
 }
