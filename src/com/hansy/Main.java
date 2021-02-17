9d16776fbfb2b2a406e4259e525516260ff758f8
Hans Accola <Hansolo6283@gmail.com>
2021-02-17 11:20:57 -0600

    primitive types challenge done

--- src/com/hansy/Main.java
+++ src/com/hansy/Main.java
@@ -18,27 +18,38 @@ public class Main {
 
         byte myMinByteValue = Byte.MIN_VALUE;
         byte myMaxByteValue = Byte.MAX_VALUE;
-        System.out.println("Byte maximum value = " + myMinByteValue);
-        System.out.println("Byte minimum value = " + myMaxByteValue);
+        System.out.println("Byte minimum value = " + myMinByteValue);
+        System.out.println("Byte maximum value = " + myMaxByteValue);
 
         short myMinShortValue = Short.MIN_VALUE;
         short myMaxShortValue = Short.MAX_VALUE;
-        System.out.println("Short maximum value = " + myMinShortValue);
-        System.out.println("Short minimum value = " + myMaxShortValue);
+        System.out.println("Short minimum value = " + myMinShortValue);
+        System.out.println("Short maximum value = " + myMaxShortValue);
         
         long myLongValue = 100L;
         long myMinLongValue = Long.MIN_VALUE;
         long myMaxLongValue = Long.MAX_VALUE;
-        System.out.println("Long maximum value = " + myMinLongValue);
-        System.out.println("Long minimum value = " + myMaxLongValue);
+        System.out.println("Long minimum value = " + myMinLongValue);
+        System.out.println("Long maximum value = " + myMaxLongValue);
         long bigLongLiteralValue = 2147483647323L;
         System.out.println(bigLongLiteralValue);
 
         short bigShortLiteralValue = 32767;
 
         int myTotal = (myMinIntValue / 2);
-        byte myNewByteValue = (byte)(myMinByteValue / 2);
+        byte myNewByteValue = (byte) (myMinByteValue / 2);
 
-        short myNewShortValue = (short)(myMinShortValue / 2);
+        short myNewShortValue = (short) (myMinShortValue / 2);
+
+        //Primitive Types Challenge
+        byte validByte = 10;
+        short validShort = 20; // _ acts as a comma
+        int validInt = 50; // _ acts as a comma
+
+        long validAnswer = 50000L + 10 * (validByte + validShort + validInt);
+
+        System.out.println("Challenge answer = " + validAnswer);
+
+        short shortTotal = (short) (1000 + 10 * (validByte + validShort + validInt));
     }
 }
